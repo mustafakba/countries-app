449451fa6d0e63219d54c9d1685505be1655ae5d
mustafakba <kaba.mustafaa@gmail.com>
2022-11-07 22:28:35 +0300

    AboutPage created

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        {{this.$store.state.country[0] ? this.$store.state.country[0].name : "" }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AboutPage",
+   data(){
+        return{
+
+        }
+   },
+    methods:{
+        firstRun(){
+        }
+    },
+    created() {
+        let name = this.$route.params.name
+        console.log(name)
+        this.$store.dispatch("getDetail",name)
+    },
+}
+
+</script>
+
+<style scoped>
+
+</style>
