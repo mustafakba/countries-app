60410eb5de6b7db433215b06dd8d9600388c74f4
mustafakba <kaba.mustafaa@gmail.com>
2022-11-07 18:31:03 +0300

    dependencies added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from "@/store"
+import router from "@/router"
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(store)
+Vue.use(router)
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
+  store,
+  router,
+
 }).$mount('#app')
