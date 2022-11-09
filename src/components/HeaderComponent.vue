67d5049ef9fc2af68e96955aeb4d54529335f3a3
mustafakba <kaba.mustafaa@gmail.com>
2022-11-09 19:04:24 +0300

    header created

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container">
+
+        <div class="row height d-flex justify-content-center align-items-center">
+
+            <div class="col-md-8">
+                <div class="search">
+                    <i class="fa fa-search"></i>
+                    <input v-model="searchedText" type="text" class="form-control" placeholder="Herhangi Bir Ülkeyi Merak Mı Ediyorsunuz ? Arayın...">
+                    <button @click="sendSearch" class="btn btn-primary">Search</button>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent",
+    data(){
+        return{
+            searchedText : '',
+        }
+    },
+    methods:{
+        sendSearch(){
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+
+.container{
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.search{
+    position: relative;
+    box-shadow: 0 0 40px rgba(51, 51, 51, .1);
+
+}
+
+.search input{
+
+    height: 60px;
+    text-indent: 25px;
+    border: 2px solid #d6d4d4;
+
+
+}
+
+
+.search input:focus{
+
+    box-shadow: none;
+    border: 2px solid blue;
+
+
+}
+
+.search .fa-search{
+
+    position: absolute;
+    top: 20px;
+    left: 16px;
+
+}
+
+.search button{
+
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    height: 50px;
+    width: 110px;
+    background: blue;
+
+}
+
+</style>
